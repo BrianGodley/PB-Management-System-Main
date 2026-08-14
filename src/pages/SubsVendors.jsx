95f970de606e0774d65388ebe752c3a43b36f3b5
Brian <brian@picturebuild.com>
2026-08-14 06:50:48 -0700

    Vendors: rename Materials tab to Master Material Rates

--- src/pages/SubsVendors.jsx
+++ src/pages/SubsVendors.jsx
@@ -908,7 +908,7 @@ export default function SubsVendors({ mode = 'sub' }) {
                   : 'border-transparent text-gray-500 hover:text-gray-700'
               }`}
             >
-              🧱 Materials
+              🧱 Master Material Rates
             </button>
           )}
 
